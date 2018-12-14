360f9eeb7be0cf00a3df7cb584a967a58f33a4b7
Avni Bhardwaj <40702176+Avnibh@users.noreply.github.com>
2018-12-15 00:42:27 +0530

    Add files via upload

---
+++ marketing-socialmediamarketing-startup.php
@@ -0,0 +1,287 @@
+<?php
+include "header.php";
+?>
+<body>
+	<!-- section -->
+	<div class="section">
+		<!-- container -->
+		<div class="container">
+			<!-- row -->
+			<div class="row">
+				<div class="col-md-8">
+					<!-- post share -->
+					<div class="section-row">
+						<div class="post-share">
+							<a href="#" class="social-facebook"><i class="fa fa-facebook"></i><span>Share</span></a>
+							<a href="#" class="social-twitter"><i class="fa fa-twitter"></i><span>Tweet</span></a>
+							<a href="#" class="social-pinterest"><i class="fa fa-pinterest"></i><span>Pin</span></a>
+							<a href="#" ><i class="fa fa-envelope"></i><span>Email</span></a>
+						</div>
+					</div>
+					<!-- /post share -->
+
+					<!-- post content -->
+					<div class="section-row">
+						<h3>YOUTH:  THE PHENOMENAL BRAIN OF START-UPS.</h3>
+						<p>In this tech-savvy generation of gadgets and smart devices, each one of is associated with technology in some form or the other. We may use different browsers, go through various websites, or use a number of applications in our smartphones. Certainly, the invasion of technical trends in our country has changed the scenario of industries and the corporate world in a great sense. It is quite evident nowadays to watch kids using smartphones and other high-tech devices smoothly. The technology has enabled each one of us to easily do whatever we want, be it personal or professional. It is getting smarter, faster and better. But ever wondered how these technologies that we use on routine basis are developed? Who is the mastermind? And what it takes to get into such technical advancements? Well, we have the answers for you right here.</p>
+						<figure class="pull-right">
+							<img src="./img/BLOGS/single.jpeg" alt="">
+							<figcaption></figcaption>
+						</figure>
+						<p>But what’s bothersome today is that India might be an early adopter of technology on the commercial front but not when it comes to educating the masses, especially the youth; about the technicalities of the services and devices they are using. The education system does not pay much heed to it. India is the second largest smartphone user after China, but why is it that the youngsters mostly invest in using smartphones and all the trending technologies and not in developing or knowing the basics of how to make it work. Or is it that we might think of doing so but feel it’s better to go with the flow? This skill-based brain is what lacks in today’s educated masses. It is maybe because of the education system that has trained us to think more logically than creatively. Our brains have been trained from the very beginning to be merely a good logical thinker than to get creative and explore new paths. There are a large number of colleges in India which claim world-class education facilities for students, but this is the one and the only superficial reality of our education system. In the name of education, the students are only imparted knowledge on the theoretical front and its applications still remain to be conserved only to specific organizations and education authorities. The fact is that technical studies are limited even in the largest of colleges in India. Our perception is shaped so as to consider a regular 9 to 5 job as the source of income for most educated people in our country. As students, we only study what our teachers teach us in colleges. We do not do anything creative about it, never try to think out of the box.                        </p>
+						<blockquote class="blockquote">
+							<p>It is rightly said “Better an Oops than a What If” because it is worth giving a try at something than fearing failure and doing nothing about it. What is your take on it? </p>
+						</blockquote>
+						<p></p>
+						<figure>
+							<img src="./img/BLOGS/blog1-fig3.jpg" alt="">
+						</figure>
+						<h3>Media Marketing</h3>
+						<p>They make online searches before any purchases. </p>
+					</div>
+					<!-- /post content -->
+
+					<!-- post tags -->
+					<div class="section-row">
+						<div class="post-tags">
+							<ul>
+								<li>TAGS:</li>
+								<li><a href="#">Social</a></li>
+								<li><a href="#">Lifestyle</a></li>
+								<li><a href="#">Fashion</a></li>
+								<li><a href="#">Health</a></li>
+							</ul>
+						</div>
+					</div>
+					<!-- /post tags -->
+
+					
+
+					<!-- post author -->
+					<div class="section-row">
+						<div class="section-title">
+							<h3 class="title">About <a href="author.html">AVNI BHARDWAJ</a></h3>
+						</div>
+						<div class="author media">
+							<div class="media-left">
+								<a href="author.html">
+									<img class="author-img media-object" src="./img/avatar-1.jpg" alt="">
+								</a>
+							</div>
+							<div class="media-body">
+								<p>WEB DEVELOPER</p>
+								<ul class="author-social">
+									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+									<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- /post author -->
+
+					<!-- /related post -->
+					<div>
+						<div class="section-title">
+							<h3 class="title">Related Posts</h3>
+						</div>
+						<div class="row">
+							<!-- post -->
+							<div class="col-md-4">
+								<div class="post post-sm">
+									<a class="post-img" href="marketing-socialmediamarketing-brandawareness.php"><img src="./img/BLOGS/brand.jpg" alt=""></a>
+									<div class="post-body">
+										<div class="post-category">
+											<a href="category.html">SOCIAL</a>
+										</div>
+										<h3 class="post-title title-sm"><a href="marketing-socialmediamarketing-brandawareness.php">Brand Awareness </a></h3>
+										<ul class="post-meta">
+											<li><a href="author.html">Avni Bhardwaj</a></li>
+											<li>20 April 2018</li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<!-- /post -->
+
+						
+
+						</div>
+					</div>
+					<!-- /related post -->
+
+					<!-- post comments -->
+					<div class="section-row">
+						<div class="section-title">
+							<h3 class="title">3 Comments</h3>
+						</div>
+						<div class="post-comments">
+							<!-- comment -->
+							<div class="media">
+								<div class="media-left">
+									<img class="media-object" src="./img/avatar-2.jpg" alt="">
+								</div>
+								<div class="media-body">
+									<div class="media-heading">
+										<h4>John Doe</h4>
+										<span class="time">5 min ago</span>
+									</div>
+									<p>This the only way to explore or publish your product?.</p>
+									<a href="#" class="reply">Reply</a>
+									<!-- comment -->
+									<div class="media media-author">
+										<div class="media-left">
+											<img class="media-object" src="./img/avatar-1.jpg" alt="">
+										</div>
+										<div class="media-body">
+											<div class="media-heading">
+												<h4>John Doe</h4>
+												<span class="time">5 min ago</span>
+											</div>
+											<p>This the only way to explore.</p>
+											<a href="#" class="reply">Reply</a>
+										</div>
+									</div>
+									<!-- /comment -->
+								</div>
+							</div>
+							<!-- /comment -->
+
+							
+						</div>
+					</div>
+					<!-- /post comments -->
+
+					<!-- post reply -->
+					<div class="section-row">
+						<div class="section-title">
+							<h3 class="title">Leave a reply</h3>
+						</div>
+						<form class="post-reply">
+							<div class="row">
+								<div class="col-md-12">
+									<div class="form-group">
+										<textarea class="input" name="message" placeholder="Message"></textarea>
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="form-group">
+										<input class="input" type="text" name="name" placeholder="Name">
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="form-group">
+										<input class="input" type="email" name="email" placeholder="Email">
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="form-group">
+										<input class="input" type="text" name="website" placeholder="Website">
+									</div>
+								</div>
+								<div class="col-md-12">
+									<button class="primary-button">Submit</button>
+								</div>
+
+							</div>
+						</form>
+					</div>
+					<!-- /post reply -->
+				</div>
+				<div class="col-md-4">
+					
+
+					<!-- social widget -->
+					<div class="aside-widget">
+						<div class="section-title">
+							<h2 class="title">Social Media</h2>
+						</div>
+						<div class="social-widget">
+							<ul>
+								<li>
+									<a href="#" class="social-facebook">
+										<i class="fa fa-facebook"></i>
+										<span>21.2K<br>Followers</span>
+									</a>
+								</li>
+								<li>
+									<a href="#" class="social-twitter">
+										<i class="fa fa-twitter"></i>
+										<span>10.2K<br>Followers</span>
+									</a>
+								</li>
+								<li>
+									<a href="#" class="social-google-plus">
+										<i class="fa fa-google-plus"></i>
+										<span>5K<br>Followers</span>
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<!-- /social widget -->
+
+					<!-- category widget -->
+					<div class="aside-widget">
+						<div class="section-title">
+							<h2 class="title">Categories</h2>
+						</div>
+						<div class="category-widget">
+							<ul>
+								<li><a href="#">Lifestyle <span>451</span></a></li>
+								<li><a href="#">Fashion <span>230</span></a></li>
+								<li><a href="#">Technology <span>40</span></a></li>
+								<li><a href="#">Travel <span>38</span></a></li>
+								<li><a href="#">Health <span>24</span></a></li>
+							</ul>
+						</div>
+					</div>
+					<!-- /category widget -->
+
+					<!-- newsletter widget -->
+					<div class="aside-widget">
+						<div class="section-title">
+							<h2 class="title">Newsletter</h2>
+						</div>
+						<div class="newsletter-widget">
+							<form>
+								<p></p>
+								<input class="input" placeholder="Enter Your Email">
+								<button class="primary-button">Subscribe</button>
+							</form>
+						</div>
+					</div>
+					<!-- /newsletter widget -->
+
+					
+
+					<!-- galery widget -->
+					<div class="aside-widget">
+						<div class="section-title">
+							<h2 class="title">Instagram</h2>
+						</div>
+						<div class="galery-widget">
+							<ul>
+								<li><a href="#"><img src="./img/galery-1.jpg" alt=""></a></li>
+								<li><a href="#"><img src="./img/galery-2.jpg" alt=""></a></li>
+								<li><a href="#"><img src="./img/galery-3.jpg" alt=""></a></li>
+								<li><a href="#"><img src="./img/galery-4.jpg" alt=""></a></li>
+								<li><a href="#"><img src="./img/galery-5.jpg" alt=""></a></li>
+								<li><a href="#"><img src="./img/galery-6.jpg" alt=""></a></li>
+							</ul>
+						</div>
+					</div>
+					<!-- /galery widget -->
+				</div>
+			</div>
+			<!-- /row -->
+		</div>
+		<!-- /container -->
+	</div>
+	<!-- /SECTION -->
+
+</body>
+<?php
+include "footer.php";
+?>
