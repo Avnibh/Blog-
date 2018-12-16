60c8e0d3b8045cd488bdec29fed55f64be8d5847
Avni Bhardwaj <40702176+Avnibh@users.noreply.github.com>
2018-12-17 00:36:27 +0530

    Add files via upload

---
+++ technology-tech-evolution.php
@@ -0,0 +1,301 @@
+<?php
+include "header.php";
+?>
+<body>
+	<!-- section -->
+	<div class="section">
+		<!-- container -->
+		<div class="container">
+			<!-- row -->
+			<div class="row">
+				<div class="col-md-8">
+					<!-- post share -->
+					<div class="section-row">
+						<div class="post-share">
+							<a href="#" class="social-facebook"><i class="fa fa-facebook"></i><span>Share</span></a>
+							<a href="#" class="social-twitter"><i class="fa fa-twitter"></i><span>Tweet</span></a>
+							<a href="#" class="social-pinterest"><i class="fa fa-pinterest"></i><span>Pin</span></a>
+							<a href="#" ><i class="fa fa-envelope"></i><span>Email</span></a>
+						</div>
+					</div>
+					<!-- /post share -->
+
+					<!-- post content -->
+					<div class="section-row">
+						<h3>Technology changes are Best or worst?
+
+</h3>
+						<p>Having a glimpse of the technical trends in the past couple of years, there have been several advancements in the field of technology.  Some changes turned out to be a boon for the industry while others proved to be unproductive. In some way or the other Technology Is Shaping Our World. The pace of technological change is rapidly accelerating & these technologies have changed our lives before we even realized it.
+
+Talking of the advantages of technology, it not only reduces human labor but also saves time and effort. For instance, the use of smartphones has changed our lives to such a great extent that we genuinely have to depend on them for every little task, be it looking up a contact or simple calculation. Other significant innovations in technology have enabled us to broaden our horizons and envision a whole new futuristic world.
+
+Tesla Hyperloop by Elon Musk, the most hyped tech topic of now would prove to be a massive revolution in technology if turns out to be a success. Another amazing innovation is The emergence of AI –especially in the form of Alexa (Amazon’s smart voice recognition) and Google Assistant. Samsung Bixby is another example. Samsung phones offer a dedicated assistant with integrated voice recognition and system controls.
+
+
+</p>
+						<figure class="pull-right">
+							<img src="./img/BLOGS/brand.png" alt="">
+							<figcaption></figcaption>
+						</figure>
+						<p>Cloud-based AI services like Amazon, Google, IBM and Microsoft, which are all working on increasing access to machine learning and artificial neural network technology, in order to make it more affordable and easy to use. Doctors and scientists are using technology to tackle problems that once seemed insurmountable.
+ 
+ The truth, however, is that many of the emerging tech fields still have a really long way to go until they have been proven to be safe and are affordable. The dark side of the technological revolution is that, it being a crucial part of existence in contemporary world, has many repercussions too. 
+ Easy and on time availability of resources over the web and ‘On-Demand Everything’ has made people Less Patient. Also, more dependence on Tech devices Leads to Less Creative Thinking in humans.
+ 
+ </p><p></p>
+ <figure>
+							<img src="./img/BLOGS/tech2.jpg" alt="">
+                        </figure>
+                        
+ <blockquote class="blockquote">
+							<p>So there is long way to go in the arena of technology to overcome these shortcomings and benefit maximum from the technology that we are surrounded by. </p>
+						</blockquote>
+					
+						
+							</div>
+					<!-- /post content -->
+
+					<!-- post tags -->
+					<div class="section-row">
+						<div class="post-tags">
+							<ul>
+								<li>TAGS:</li>
+								<li><a href="#">Technology</a></li>
+								<li><a href="#">Evoltion</a></li>
+								<li><a href="#">Trends</a></li>
+								<li><a href="#">Tech</a></li>
+							</ul>
+						</div>
+					</div>
+					<!-- /post tags -->
+
+					
+
+					<!-- post author -->
+					<div class="section-row">
+						<div class="section-title">
+							<h3 class="title">About <a href="author.html">AVNI BHARDWAJ</a></h3>
+						</div>
+						<div class="author media">
+							<div class="media-left">
+								<a href="author.html">
+									<img class="author-img media-object" src="./img/avatar-1.jpg" alt="">
+								</a>
+							</div>
+							<div class="media-body">
+								<p>WEB DEVELOPER</p>
+								<ul class="author-social">
+									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+									<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- /post author -->
+
+					<!-- /related post -->
+					<div>
+						<div class="section-title">
+							<h3 class="title">Related Posts</h3>
+						</div>
+						<div class="row">
+							<!-- post -->
+							<div class="col-md-4">
+								<div class="post post-sm">
+									<a class="post-img" href="marketing-socialmediamarketing-brandawareness.php"><img src="./img/BLOGS/brand.jpg" alt=""></a>
+									<div class="post-body">
+										<div class="post-category">
+											<a href="category.html">SOCIAL</a>
+										</div>
+										<h3 class="post-title title-sm"><a href="marketing-socialmediamarketing-brandawareness.php">Brand Awareness </a></h3>
+										<ul class="post-meta">
+											<li><a href="author.html">Avni Bhardwaj</a></li>
+											<li></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<!-- /post -->
+
+						
+
+						</div>
+					</div>
+					<!-- /related post -->
+
+					<!-- post comments -->
+					<div class="section-row">
+						<div class="section-title">
+							<h3 class="title">3 Comments</h3>
+						</div>
+						<div class="post-comments">
+							<!-- comment -->
+							<div class="media">
+								<div class="media-left">
+									<img class="media-object" src="./img/avatar-2.jpg" alt="">
+								</div>
+								<div class="media-body">
+									<div class="media-heading">
+										<h4>John Doe</h4>
+										<span class="time">5 min ago</span>
+									</div>
+									<p>This article was Knowledgeable</p>
+									<a href="#" class="reply">Reply</a>
+									<!-- comment -->
+									<div class="media media-author">
+										<div class="media-left">
+											<img class="media-object" src="./img/avatar-1.jpg" alt="">
+										</div>
+										<div class="media-body">
+											<div class="media-heading">
+												<h4>Jamie Dove</h4>
+												<span class="time">5 min ago</span>
+											</div>
+											<p>This the only way to explore.</p>
+											<a href="#" class="reply">Reply</a>
+										</div>
+									</div>
+									<!-- /comment -->
+								</div>
+							</div>
+							<!-- /comment -->
+
+							
+						</div>
+					</div>
+					<!-- /post comments -->
+
+					<!-- post reply -->
+					<div class="section-row">
+						<div class="section-title">
+							<h3 class="title">Leave a reply</h3>
+						</div>
+						<form class="post-reply">
+							<div class="row">
+								<div class="col-md-12">
+									<div class="form-group">
+										<textarea class="input" name="message" placeholder="Message"></textarea>
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="form-group">
+										<input class="input" type="text" name="name" placeholder="Name">
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="form-group">
+										<input class="input" type="email" name="email" placeholder="Email">
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="form-group">
+										<input class="input" type="text" name="website" placeholder="Website">
+									</div>
+								</div>
+								<div class="col-md-12">
+									<button class="primary-button">Submit</button>
+								</div>
+
+							</div>
+						</form>
+					</div>
+					<!-- /post reply -->
+				</div>
+				<div class="col-md-4">
+					
+
+					<!-- social widget -->
+					<div class="aside-widget">
+						<div class="section-title">
+							<h2 class="title">Social Media</h2>
+						</div>
+						<div class="social-widget">
+							<ul>
+								<li>
+									<a href="#" class="social-facebook">
+										<i class="fa fa-facebook"></i>
+										<span>21.2K<br>Followers</span>
+									</a>
+								</li>
+								<li>
+									<a href="#" class="social-twitter">
+										<i class="fa fa-twitter"></i>
+										<span>10.2K<br>Followers</span>
+									</a>
+								</li>
+								<li>
+									<a href="#" class="social-google-plus">
+										<i class="fa fa-google-plus"></i>
+										<span>5K<br>Followers</span>
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<!-- /social widget -->
+
+					<!-- category widget -->
+					<div class="aside-widget">
+						<div class="section-title">
+							<h2 class="title">Categories</h2>
+						</div>
+						<div class="category-widget">
+							<ul>
+								<li><a href="#">Lifestyle <span>451</span></a></li>
+								<li><a href="#">Fashion <span>230</span></a></li>
+								<li><a href="#">Technology <span>40</span></a></li>
+								<li><a href="#">Travel <span>38</span></a></li>
+								<li><a href="#">Health <span>24</span></a></li>
+							</ul>
+						</div>
+					</div>
+					<!-- /category widget -->
+
+					<!-- newsletter widget -->
+					<div class="aside-widget">
+						<div class="section-title">
+							<h2 class="title">Newsletter</h2>
+						</div>
+						<div class="newsletter-widget">
+							<form>
+								<p></p>
+								<input class="input" placeholder="Enter Your Email">
+								<button class="primary-button">Subscribe</button>
+							</form>
+						</div>
+					</div>
+					<!-- /newsletter widget -->
+
+					
+
+					<!-- galery widget -->
+					<div class="aside-widget">
+						<div class="section-title">
+							<h2 class="title">Instagram</h2>
+						</div>
+						<div class="galery-widget">
+							<ul>
+								<li><a href="#"><img src="./img/galery-1.jpg" alt=""></a></li>
+								<li><a href="#"><img src="./img/galery-2.jpg" alt=""></a></li>
+								<li><a href="#"><img src="./img/galery-3.jpg" alt=""></a></li>
+								<li><a href="#"><img src="./img/galery-4.jpg" alt=""></a></li>
+								<li><a href="#"><img src="./img/galery-5.jpg" alt=""></a></li>
+								<li><a href="#"><img src="./img/galery-6.jpg" alt=""></a></li>
+							</ul>
+						</div>
+					</div>
+					<!-- /galery widget -->
+				</div>
+			</div>
+			<!-- /row -->
+		</div>
+		<!-- /container -->
+	</div>
+	<!-- /SECTION -->
+
+</body>
+<?php
+include "footer.php";
+?>
