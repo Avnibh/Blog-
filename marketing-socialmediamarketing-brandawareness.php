593fe14d4ca5c02d25ba5af1f044d09cb7b130bd
Avni Bhardwaj <40702176+Avnibh@users.noreply.github.com>
2018-12-16 22:25:43 +0530

    Add files via upload

---
+++ marketing-socialmediamarketing-brandawareness.php
@@ -0,0 +1,285 @@
+<?php
+include "header.php";
+?>
+<body>
+	<!-- section -->
+	<div class="section">
+		<!-- container -->
+		<div class="container">
+			<!-- row -->
+			<div class="row">
+				<div class="col-md-8">
+					<!-- post share -->
+					<div class="section-row">
+						<div class="post-share">
+							<a href="#" class="social-facebook"><i class="fa fa-facebook"></i><span>Share</span></a>
+							<a href="#" class="social-twitter"><i class="fa fa-twitter"></i><span>Tweet</span></a>
+							<a href="#" class="social-pinterest"><i class="fa fa-pinterest"></i><span>Pin</span></a>
+							<a href="#" ><i class="fa fa-envelope"></i><span>Email</span></a>
+						</div>
+					</div>
+					<!-- /post share -->
+
+					<!-- post content -->
+					<div class="section-row">
+						<h3>Social Media Marketing: Way to Increase Your Brand Awareness </h3>
+						<p>If we talk about social media, in today’s world it is the most cost-efficient way for digital marketing. Social Media Marketing is used to promote products in the market and increase brand visibility through social handles. 
+The most effective approach to increase brand awareness is to implement a social media strategy which significantly hikes up brand recognition and encourages wider customer audience.
+Now, How We Can Do it?
+</p>
+						<p>For this, you need to create social media business profiles and begin interacting with public on social platforms through posting and blogging. It will not only benefit in building your professional networks but also help get employees, business partners and sponsors to like, comment and Share your page. This will fetch you a decent number of people in your support, which will ultimately increase your brand awareness and aid in building reputation in the market. As a result, your business and brand will have place in people’s minds, which will surely favor the growth and development on a large scale. 
+Talking about the social marketing using these platforms, each new post that is shared to the public gets introduced to a newer network of individuals every time, which might as well lead them to  become your customers in future. More the people get to know about the business, more is the client- base broadened which helps get much more satisfied customers and benefits the business.
+All you need is a mindful strategy to connect to the target audience through online content. Over 91% of marketers and businessmen claimed that their investments in social marketing strategies significantly increased business and their brand exposure. The fact is, that by simply creating business pages on various social handles and updating them on regular basis, one can grasp the attention of targeted audience and thus huge customers for their business. 
+This is contemporarily the most operative approach to spread brand awareness worldwide.
+ </p><p></p>
+ <figure>
+							<img src="./img/BLOGS/smedia.jpg" alt="social">
+						</figure>
+													
+					</div>
+					<!-- /post content -->
+
+					<!-- post tags -->
+					<div class="section-row">
+						<div class="post-tags">
+							<ul>
+								<li>TAGS:</li>
+								<li><a href="#">Social</a></li>
+								<li><a href="#">Lifestyle</a></li>
+								<li><a href="#">Fashion</a></li>
+								<li><a href="#">Health</a></li>
+							</ul>
+						</div>
+					</div>
+					<!-- /post tags -->
+
+					
+
+					<!-- post author -->
+					<div class="section-row">
+						<div class="section-title">
+							<h3 class="title">About <a href="author.html">AVNI BHARDWAJ</a></h3>
+						</div>
+						<div class="author media">
+							<div class="media-left">
+								<a href="author.html">
+									<img class="author-img media-object" src="./img/avatar-1.jpg" alt="">
+								</a>
+							</div>
+							<div class="media-body">
+								<p>WEB DEVELOPER</p>
+								<ul class="author-social">
+									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+									<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- /post author -->
+
+					<!-- /related post -->
+					<div>
+						<div class="section-title">
+							<h3 class="title">Related Posts</h3>
+						</div>
+						<div class="row">
+							<!-- post -->
+							<div class="col-md-4">
+								<div class="post post-sm">
+									<a class="post-img" href="marketing-socialmediamarketing-startup.php"><img src="./img/BLOGS/blog1-fig3.jpg" alt=""></a>
+									<div class="post-body">
+										<div class="post-category">
+											<a href="category.html">Startups</a>
+										</div>
+										<h3 class="post-title title-sm"><a href="marketing-socialmediamarketing-startup.php">Youth And Startup.</a></h3>
+										<ul class="post-meta">
+											<li><a href="author.html">Avni Bhardwaj</a></li>
+											<li></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<!-- /post -->
+
+						
+
+						</div>
+					</div>
+					<!-- /related post -->
+
+					<!-- post comments -->
+					<div class="section-row">
+						<div class="section-title">
+							<h3 class="title">3 Comments</h3>
+						</div>
+						<div class="post-comments">
+							<!-- comment -->
+							<div class="media">
+								<div class="media-left">
+									<img class="media-object" src="./img/avatar-2.jpg" alt="">
+								</div>
+								<div class="media-body">
+									<div class="media-heading">
+										<h4>John Doe</h4>
+										<span class="time">5 min ago</span>
+									</div>
+									<p>This article was Knowledgeable</p>
+									<a href="#" class="reply">Reply</a>
+									<!-- comment -->
+									<div class="media media-author">
+										<div class="media-left">
+											<img class="media-object" src="./img/avatar-1.jpg" alt="">
+										</div>
+										<div class="media-body">
+											<div class="media-heading">
+												<h4>Jamie Dove</h4>
+												<span class="time">5 min ago</span>
+											</div>
+											<p>This the only way to explore.</p>
+											<a href="#" class="reply">Reply</a>
+										</div>
+									</div>
+									<!-- /comment -->
+								</div>
+							</div>
+							<!-- /comment -->
+
+							
+						</div>
+					</div>
+					<!-- /post comments -->
+
+					<!-- post reply -->
+					<div class="section-row">
+						<div class="section-title">
+							<h3 class="title">Leave a reply</h3>
+						</div>
+						<form class="post-reply">
+							<div class="row">
+								<div class="col-md-12">
+									<div class="form-group">
+										<textarea class="input" name="message" placeholder="Message"></textarea>
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="form-group">
+										<input class="input" type="text" name="name" placeholder="Name">
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="form-group">
+										<input class="input" type="email" name="email" placeholder="Email">
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="form-group">
+										<input class="input" type="text" name="website" placeholder="Website">
+									</div>
+								</div>
+								<div class="col-md-12">
+									<button class="primary-button">Submit</button>
+								</div>
+
+							</div>
+						</form>
+					</div>
+					<!-- /post reply -->
+				</div>
+				<div class="col-md-4">
+					
+
+					<!-- social widget -->
+					<div class="aside-widget">
+						<div class="section-title">
+							<h2 class="title">Social Media</h2>
+						</div>
+						<div class="social-widget">
+							<ul>
+								<li>
+									<a href="#" class="social-facebook">
+										<i class="fa fa-facebook"></i>
+										<span>21.2K<br>Followers</span>
+									</a>
+								</li>
+								<li>
+									<a href="#" class="social-twitter">
+										<i class="fa fa-twitter"></i>
+										<span>10.2K<br>Followers</span>
+									</a>
+								</li>
+								<li>
+									<a href="#" class="social-google-plus">
+										<i class="fa fa-google-plus"></i>
+										<span>5K<br>Followers</span>
+									</a>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<!-- /social widget -->
+
+					<!-- category widget -->
+					<div class="aside-widget">
+						<div class="section-title">
+							<h2 class="title">Categories</h2>
+						</div>
+						<div class="category-widget">
+							<ul>
+								<li><a href="#">Lifestyle <span>451</span></a></li>
+								<li><a href="#">Fashion <span>230</span></a></li>
+								<li><a href="#">Technology <span>40</span></a></li>
+								<li><a href="#">Travel <span>38</span></a></li>
+								<li><a href="#">Health <span>24</span></a></li>
+							</ul>
+						</div>
+					</div>
+					<!-- /category widget -->
+
+					<!-- newsletter widget -->
+					<div class="aside-widget">
+						<div class="section-title">
+							<h2 class="title">Newsletter</h2>
+						</div>
+						<div class="newsletter-widget">
+							<form>
+								<p></p>
+								<input class="input" placeholder="Enter Your Email">
+								<button class="primary-button">Subscribe</button>
+							</form>
+						</div>
+					</div>
+					<!-- /newsletter widget -->
+
+					
+
+					<!-- galery widget -->
+					<div class="aside-widget">
+						<div class="section-title">
+							<h2 class="title">Instagram</h2>
+						</div>
+						<div class="galery-widget">
+							<ul>
+								<li><a href="#"><img src="./img/galery-1.jpg" alt=""></a></li>
+								<li><a href="#"><img src="./img/galery-2.jpg" alt=""></a></li>
+								<li><a href="#"><img src="./img/galery-3.jpg" alt=""></a></li>
+								<li><a href="#"><img src="./img/galery-4.jpg" alt=""></a></li>
+								<li><a href="#"><img src="./img/galery-5.jpg" alt=""></a></li>
+								<li><a href="#"><img src="./img/galery-6.jpg" alt=""></a></li>
+							</ul>
+						</div>
+					</div>
+					<!-- /galery widget -->
+				</div>
+			</div>
+			<!-- /row -->
+		</div>
+		<!-- /container -->
+	</div>
+	<!-- /SECTION -->
+
+</body>
+<?php
+include "footer.php";
+?>
